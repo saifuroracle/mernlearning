1964585c88130b3722fdfab69a74468bb6f81910
saifur rahman <saifuroracle@gmail.com>
2020-12-06 13:27:47 +0600

    express static route added

--- express/1/index.js
+++ express/1/index.js
@@ -3,12 +3,11 @@ const { dirname } = require('path');
 const path = require('path')
 const app = express();
 
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-});
-
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
