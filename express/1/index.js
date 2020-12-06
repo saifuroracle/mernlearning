1cfb1cc8637811a05683f872109c61e32d703364
saifur rahman <saifuroracle@gmail.com>
2020-12-06 12:41:48 +0600

    express public home page added

--- express/1/index.js
+++ express/1/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const { dirname } = require('path');
+const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello World!</h1>`);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 app.listen(PORT, () => {
