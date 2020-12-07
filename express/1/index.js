db57173abf13bee9037bd2438d08fbac5dcb5058
saifur rahman <saifuroracle@gmail.com>
2020-12-07 12:35:24 +0600

    express 1 => api members api seperated

--- express/1/index.js
+++ express/1/index.js
@@ -2,33 +2,12 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const logger = require("./middleware/logger")
-const members = require("./data/members")
 
 // init middlware
 app.use(logger);
 
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
-
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(
-        member => 
-            member.id == parseInt(req.params.id)
-    );
-
-    if (found) {
-        res.json(
-            members.filter(member =>
-                member.id == parseInt(req.params.id)
-            )
-        );
-    } else {
-        res.status(400).json({
-            "msg": "Member Not Found!"
-        });
-    }
-});
+// routes
+app.use('/api/members', require('./routes/api/members'));
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
