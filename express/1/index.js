de51e474c38ff6ffac17e7a7305799c642f8226b
saifur rahman <saifuroracle@gmail.com>
2020-12-07 12:18:59 +0600

    express 1 => api with error msg

--- express/1/index.js
+++ express/1/index.js
@@ -12,12 +12,22 @@ app.get('/api/members', (req, res) => {
 });
 
 app.get('/api/members/:id', (req, res) => {
-    
-    res.json(
-        members.filter(member =>
+    const found = members.some(
+        member => 
             member.id == parseInt(req.params.id)
-        )
     );
+
+    if (found) {
+        res.json(
+            members.filter(member =>
+                member.id == parseInt(req.params.id)
+            )
+        );
+    } else {
+        res.status(400).json({
+            "msg": "Member Not Found!"
+        });
+    }
 });
 
 // set static folder
