7959326bd8b8aac2dd09a776dab0d74fd122c4bd
saifur rahman <saifuroracle@gmail.com>
2020-12-09 14:43:05 +0600

    mongodb connected

--- express/reactapp/backend/server.js
+++ express/reactapp/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require("mongoose");
+
 
 require('dotenv').config()
 
@@ -9,6 +11,16 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json( ));
+
+
+// mongoose connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Mongodb successfully connected!'); 
+})
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
